6a71c04664ea19356cd30aa657d72a47d09fb104
wonjjee <yoteamo7@naver.com>
2024-02-16 12:17:40 +0900

    feat: added Employee entity

--- src/entities/department/department.entity.ts
+++ src/entities/department/department.entity.ts
@@ -1,5 +1,13 @@
-import { Entity, PrimaryColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
+import {
+  Entity,
+  PrimaryColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  OneToMany,
+} from 'typeorm';
 import { Location } from '../location/location.entity';
+import { Employee } from '../employee/employee.entity';
 
 @Entity('departments')
 export class Department {
@@ -12,7 +20,17 @@ export class Department {
   @Column({ type: 'int', unsigned: true, nullable: true })
   locationId: number;
 
+  @Column({ type: 'int', unsigned: true, nullable: true })
+  managerId: number;
+
+  @OneToMany(() => Employee, (employee) => employee.Department)
+  Employees: Employee[];
+
   @ManyToOne(() => Location, (location) => location.Departments)
   @JoinColumn([{ name: 'location_id', referencedColumnName: 'locationId' }])
   Location: Location;
+
+  @ManyToOne(() => Employee, (employee) => employee.ManagedDepartments)
+  @JoinColumn([{ name: 'manager_id', referencedColumnName: 'employeeId' }])
+  Manager: Employee;
 }
---
+++ src/entities/employee/employee.entity.ts
@@ -0,0 +1,59 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryColumn,
+} from 'typeorm';
+import { Department } from '../department/department.entity';
+
+@Entity('employees')
+export class Employee {
+  @PrimaryColumn({ type: 'int', unsigned: true })
+  employeeId: number;
+
+  @Column({ type: 'varchar', length: 20, nullable: true })
+  firstName: string;
+
+  @Column({ type: 'varchar', length: 25 })
+  lastName: string;
+
+  @Column({ type: 'varchar', length: 25 })
+  email: string;
+
+  @Column({ type: 'varchar', length: 20, nullable: true })
+  phoneNumber: string;
+
+  @Column({ type: 'date' })
+  hireDate: Date;
+
+  @Column({ type: 'varchar', length: 10 })
+  jobId: string;
+
+  @Column({ type: 'decimal', precision: 8, scale: 2 })
+  salary: number;
+
+  @Column({ type: 'decimal', precision: 2, scale: 2, nullable: true })
+  commissionPct: number;
+
+  @Column({ type: 'int', unsigned: true, nullable: true })
+  managerId: number;
+
+  @Column({ type: 'int', unsigned: true, nullable: true })
+  departmentId: number;
+
+  @OneToMany(() => Employee, (employee) => employee.Manager)
+  ManagedEmployees: Employee[];
+
+  @OneToMany(() => Department, (department) => department.Manager)
+  ManagedDepartments: Department[];
+
+  @ManyToOne(() => Department, (department) => department.Employees)
+  @JoinColumn([{ name: 'department_id', referencedColumnName: 'departmentId' }])
+  Department: Department;
+
+  @ManyToOne(() => Employee, (manager) => manager.ManagedEmployees)
+  @JoinColumn([{ name: 'manager_id', referencedColumnName: 'employeeId' }])
+  Manager: Employee;
+}
