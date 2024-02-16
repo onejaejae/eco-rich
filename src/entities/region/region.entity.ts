3e4803ea023890d5bd66a8d8677ea4eabc9fdf3b
wonjjee <yoteamo7@naver.com>
2024-02-16 11:30:58 +0900

    fix: replace PrimaryColumn in region entity

--- src/entities/region/region.entity.ts
+++ src/entities/region/region.entity.ts
@@ -1,9 +1,9 @@
 import { BaseEntity } from 'src/core/database/typeorm/base.entity';
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, Column, PrimaryColumn } from 'typeorm';
 
 @Entity('regions')
 export class Region extends BaseEntity {
-  @PrimaryGeneratedColumn({ type: 'int', unsigned: true })
+  @PrimaryColumn({ type: 'int', unsigned: true })
   regionId: number;
 
   @Column({ type: 'varchar', length: 25, nullable: true })
