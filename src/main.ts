6c68da7fead08cb759e48c3797770b0104a4fb14
wonjjee <yoteamo7@naver.com>
2024-02-17 15:03:26 +0900

    feat: added setNestApp file for convenient test env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { Modules } from './modules';
+import { setNestApp } from 'setNestApp';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { EcoConfigService } from './core/config/config.service';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(Modules);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(Modules);
+
+  const configService = app.get(EcoConfigService);
+  const appConfig = configService.getAppConfig();
+
+  setNestApp(app);
+
+  await app.listen(appConfig.PORT);
+  Logger.log(`🧐 [THINGS-API][${appConfig.ENV}] Started at: ${Date.now()}`);
+  Logger.log(`🚀 Server open at ${appConfig.BASE_URL}:${appConfig.PORT}`);
 }
 bootstrap();
