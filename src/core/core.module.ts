e0a34a1dd5eff5acdeeda31948542ef99940b877
wonjjee <yoteamo7@naver.com>
2024-02-17 15:03:58 +0900

    feat: added GenericTypeOrmRepository for abstract

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,13 +1,20 @@
-import { Global, Module } from '@nestjs/common';
+import { Global, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { EcoConfigModule } from './config/config.module';
 import { getTypeOrmModule } from './database/typeorm/typeorm.module';
+import { TransactionMiddleware } from './middleware/transaction.middleware';
+import { TransactionManager } from './database/typeorm/transaction.manager';
 
 const modules = [EcoConfigModule];
+const providers = [TransactionManager];
 
 @Global()
 @Module({
   imports: [getTypeOrmModule(), ...modules],
-  providers: [],
-  exports: [...modules],
+  providers: [...providers],
+  exports: [...modules, ...providers],
 })
-export class CoreModule {}
+export class CoreModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(TransactionMiddleware).forRoutes('*');
+  }
+}
