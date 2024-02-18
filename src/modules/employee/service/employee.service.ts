f955d675f2aebc3611dfb617331576dd190bc06c
wonjjee <yoteamo7@naver.com>
2024-02-18 14:46:59 +0900

    feat: added unExist exception validation in employee service

--- src/modules/employee/service/employee.service.ts
+++ src/modules/employee/service/employee.service.ts
@@ -10,12 +10,14 @@ export class EmployeeService {
   constructor(private readonly employeeRepository: EmployeeRepository) {}
 
   async getEmployee(employeeId: number): Promise<GetEmployee> {
+    await this.employeeRepository.findOneOrThrow({ employeeId });
     return this.employeeRepository.getEmployee(employeeId);
   }
 
   async getEmployeeJobHistory(
     employeeId: number,
   ): Promise<GetEmployeeJobHistory> {
+    await this.employeeRepository.findOneOrThrow({ employeeId });
     return this.employeeRepository.getEmployeeJobHistory(employeeId);
   }
 }
