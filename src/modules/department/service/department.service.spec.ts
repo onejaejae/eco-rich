d7e02cefa7bc238018d4a6655d168116d182036f
wonjjee <yoteamo7@naver.com>
2024-02-19 12:51:00 +0900

    feat: added department service test code

--- src/modules/department/service/department.service.spec.ts
+++ src/modules/department/service/department.service.spec.ts
@@ -6,9 +6,7 @@ import {
   ECO_NAMESPACE,
 } from 'src/common/constant/nameSpace';
 import { BadRequestException } from '@nestjs/common';
-import { IEmployeeService } from '../service/employee-service.interface';
 import { EmployeeRepository } from 'src/entities/employee/employee.repository';
-import { EmployeeService } from '../service/employee.service';
 import { InitFactory } from 'test/factory/init-factory';
 import { Region } from 'src/entities/region/region.entity';
 import { Country } from 'src/entities/country/country.entity';
@@ -18,13 +16,17 @@ import { Job } from 'src/entities/job/job.entity';
 import { Employee } from 'src/entities/employee/employee.entity';
 import { JobHistory } from 'src/entities/job-history/job-history.entity';
 import { DatabaseModule } from 'test/db.module';
+import { IDepartmentService } from './department-service.interface';
+import { DepartmentRepository } from 'src/entities/department/department.repository';
+import { DepartmentService } from './department.service';
+import { UpdateDepartmentSalaryDto } from 'src/common/request/department/update-department-salary.dto';
 
-describe('employee service test', () => {
+describe('department service test', () => {
   // for testContainers
   jest.setTimeout(300_000);
 
   let dataSource: DataSource;
-  let service: IEmployeeService;
+  let service: IDepartmentService;
   let namespace: Namespace;
   let initFactory: InitFactory;
 
@@ -40,8 +42,10 @@ describe('employee service test', () => {
     ]);
 
     const txManager = new TransactionManager();
+    const departmentRepository = new DepartmentRepository(txManager);
     const employeeRepository = new EmployeeRepository(txManager);
-    service = new EmployeeService(employeeRepository);
+
+    service = new DepartmentService(departmentRepository, employeeRepository);
     initFactory = new InitFactory();
   });
 
@@ -64,77 +68,84 @@ describe('employee service test', () => {
     expect(service).toBeDefined();
   });
 
-  describe('getEmployee', () => {
-    it('Employee가 존재하지 않는 경우 - 실패', async () => {
+  describe('getDepartmentDetail', () => {
+    it('department가 존재하지 않는 경우 - 실패', async () => {
       //given
-      const unExistEmployeeId = 300;
+      const unExistDepartmentId = 300;
 
       //when
       //then
       await expect(
         namespace.runPromise(async () => {
           namespace.set(ECO_ENTITY_MANAGER, dataSource.createEntityManager());
-          await service.getEmployee(unExistEmployeeId);
+          await service.getDepartmentDetail(unExistDepartmentId);
         }),
       ).rejects.toThrow(
-        new BadRequestException(`don't exist employeeId: ${unExistEmployeeId}`),
+        new BadRequestException(
+          `don't exist departmentId: ${unExistDepartmentId}`,
+        ),
       );
     });
 
-    it('Employee가 존재하는 경우 - 성공', async () => {
+    it('deaprtment 존재하는 경우 - 성공', async () => {
       //given
       await initFactory.run(dataSource);
-      const employeeId = 100;
+      const departmentId = 10;
 
       //when
-
       const res = await namespace.runPromise(async () => {
         namespace.set(ECO_ENTITY_MANAGER, dataSource.createEntityManager());
-        return service.getEmployee(employeeId);
+        return service.getDepartmentDetail(departmentId);
       });
 
       //then
-      expect(res.Department).toBeDefined();
-      expect(res.Job).toBeDefined();
-      expect(res.ManagedEmployees).toBeDefined();
-      expect(res.ManagedDepartments).toBeDefined();
-      expect(res.Manager).toBeDefined();
+      expect(res.Location).toBeDefined();
+      expect(res.Location.Country).toBeDefined();
+      expect(res.Location.Country.Region).toBeDefined();
     });
   });
 
-  describe('getEmployeeJobHistory', () => {
-    it('Employee가 존재하지 않는 경우 - 실패', async () => {
+  describe('updateDepartmentSalary', () => {
+    it('department가 존재하지 않는 경우 - 실패', async () => {
       //given
-      const unExistEmployeeId = 300;
+      const unExistDepartmentId = 300;
 
       //when
       //then
       await expect(
         namespace.runPromise(async () => {
           namespace.set(ECO_ENTITY_MANAGER, dataSource.createEntityManager());
-          await service.getEmployeeJobHistory(unExistEmployeeId);
+          await service.getDepartmentDetail(unExistDepartmentId);
         }),
       ).rejects.toThrow(
-        new BadRequestException(`don't exist employeeId: ${unExistEmployeeId}`),
+        new BadRequestException(
+          `don't exist departmentId: ${unExistDepartmentId}`,
+        ),
       );
     });
 
-    it('Employee가 존재하는 경우 - 성공', async () => {
+    it('특정 부서의 급여를 특정 비율로 인상 및 사원 정보 업데이트  - 성공', async () => {
       //given
       await initFactory.run(dataSource);
-      const employeeId = 100;
+      const beforeSalary = 24000;
+      const departmentId = 10;
+      const updateDepartmentSalaryDto = new UpdateDepartmentSalaryDto();
+      updateDepartmentSalaryDto.increaseRate = 0.5;
 
       //when
-
       const res = await namespace.runPromise(async () => {
         namespace.set(ECO_ENTITY_MANAGER, dataSource.createEntityManager());
-        return service.getEmployeeJobHistory(employeeId);
+        return service.updateDepartmentSalary(
+          departmentId,
+          updateDepartmentSalaryDto,
+        );
       });
 
       //then
-      expect(res.JobHistories).toBeDefined();
-      expect(res.JobHistories[0].Job).toBeDefined();
-      expect(res.JobHistories[0].Department).toBeDefined();
+      expect(res[0].salary).not.toBe(beforeSalary);
+      expect(beforeSalary * (1 + updateDepartmentSalaryDto.increaseRate)).toBe(
+        res[0].salary,
+      );
     });
   });
 });
