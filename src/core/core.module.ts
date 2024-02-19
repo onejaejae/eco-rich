bac3b1a8a66bda2cea011afdd8d845401dd0b6fc
wonjjee <yoteamo7@naver.com>
2024-02-19 11:49:36 +0900

    feat: added ApiResponseInterceptor & apply interceptor, filter

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,16 +1,33 @@
-import { Global, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import {
+  ClassProvider,
+  Global,
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+} from '@nestjs/common';
 import { EcoConfigModule } from './config/config.module';
 import { getTypeOrmModule } from './database/typeorm/typeorm.module';
 import { TransactionMiddleware } from './middleware/transaction.middleware';
 import { TransactionManager } from './database/typeorm/transaction.manager';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { ErrorInterceptor } from './interceptor/error.interceptor';
+import { ApiResponseInterceptor } from './interceptor/apiResponse.interceptor';
+import { TypeORMExceptionFilter } from './filter/typeorm.exception.filter';
 
 const modules = [EcoConfigModule];
 const providers = [TransactionManager];
+const interceptors: ClassProvider[] = [
+  { provide: APP_INTERCEPTOR, useClass: ErrorInterceptor },
+  { provide: APP_INTERCEPTOR, useClass: ApiResponseInterceptor },
+];
+const filters: ClassProvider[] = [
+  { provide: APP_FILTER, useClass: TypeORMExceptionFilter },
+];
 
 @Global()
 @Module({
   imports: [getTypeOrmModule(), ...modules],
-  providers: [...providers],
+  providers: [...providers, ...interceptors, ...filters],
   exports: [...modules, ...providers],
 })
 export class CoreModule implements NestModule {
