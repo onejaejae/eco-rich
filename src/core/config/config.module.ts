db29a33d82cc803c25ce667ea8109c8ced8867d2
wonjjee <yoteamo7@naver.com>
2024-02-20 12:15:23 +0900

    feat: added env

--- src/core/config/config.module.ts
+++ src/core/config/config.module.ts
@@ -2,18 +2,12 @@ import { Global, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { configurations } from './configuration';
 import { EcoConfigService } from './config.service';
-import * as path from 'path';
-
-const envFilePath =
-  process.env.NODE_ENV === 'local'
-    ? `dotenv/.env.${process.env.NODE_ENV}`
-    : `${path.join(__dirname + '../../../.env')}`;
 
 @Global()
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: [envFilePath],
+      envFilePath: [`dotenv/.env.${process.env.NODE_ENV}`],
       load: [configurations],
     }),
   ],
